3421a07074b5bc5ae77c2cfae137252ff7672ea0
Thom van den Akker <thodor20@gmail.com>
2022-11-21 20:42:10 +0100

    Fix worker image rendering (#926)

--- pages/src/_plugins/filters/workers/worker_images.rb
+++ pages/src/_plugins/filters/workers/worker_images.rb
@@ -2,21 +2,31 @@ require_relative "../../utils/worker_utils.rb"
 
 module Jekyll
     module WorkerImagesFilter
-        def get_worker_images(input, worker, style, className = "")
+        def get_worker_images(input, worker, className = "")
             site = Jekyll.sites.first
-            path = "/assets/images/workers/#{worker}/#{style}/male"
-            dest_path = site.dest + path
+            path = "/assets/images/workers/#{worker}"
+            source_path = site.source + path
+
+            # Get the worker info
+            worker_info = WorkerUtils.getWorkerInfo(site, worker)
 
             # Get all of the source files
-            dest_images = Dir.glob(dest_path + '/**/*').select { |path| File.file?(path) }
+            source_images = Dir.glob(source_path + '/**/*').select do |path|
+                isFile = File.file?(path)
+                pieces = File.basename(path, '.*').split('-')
+                # Filter by male only because we do not want to get duplicate entries for both male and female
+                next unless isFile && worker == pieces[0] && pieces[1] == "male"
+                true
+            end
 
             # Transform the images into the proper HTML string
-            return dest_images.collect do |worker_image|
-                worker_info = WorkerUtils.getWorkerInfo(site, worker)
+            return source_images.collect do |worker_image|
+                pieces = File.basename(worker_image, '.*').split('-')
+                ext = File.extname(worker_image)
 
-                "<div class='#{className}'><div>" +
-                    "<img src='../../assets/images/workers/#{worker}/#{style}/male/#{File.basename(worker_image)}' alt='#{worker_info["name"]} male #{File.basename(worker_image, ".*")}'/></div><div>" +
-                    "<img src='../../assets/images/workers/#{worker}/#{style}/female/#{File.basename(worker_image)}' alt='#{worker_info["name"]} female #{File.basename(worker_image, ".*")}'/></div></div>"
+                "<div class='#{className}' data-style='#{pieces[2]}'><div>" +
+                    "<img src='../../assets/images/workers/#{worker}/#{pieces[2]}/male/#{pieces[3]}#{ext}' alt='#{worker_info["name"]} male #{pieces[3]}'/></div><div>" +
+                    "<img src='../../assets/images/workers/#{worker}/#{pieces[2]}/female/#{pieces[3]}#{ext}' alt='#{worker_info["name"]} female #{pieces[3]}'/></div></div>"
             end
         end
     end
