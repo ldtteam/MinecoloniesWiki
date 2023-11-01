5ad21d79f77efa2c221843b685e3a77c0b8e5c6f
Thom van den Akker <thodor20@gmail.com>
2023-11-01 22:25:03 +0100

    Fix all building info

--- public/scripts/bootstrap-init.ts
+++
@@ -1,8 +0,0 @@
-import * as bootstrap from 'bootstrap';
-// window.bootstrap = bootstrap;
-
-function loadTooltips() {
-  const tooltipList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-  tooltipList.forEach((element) => new bootstrap.Tooltip(element));
-}
-document.addEventListener('astro:page-load', loadTooltips);
