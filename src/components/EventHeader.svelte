93213eaf8fcb935210c9231137330333fdc914ec
Thom van den Akker <thodor20@gmail.com>
2023-12-23 22:52:50 +0100

    Update events pages

---
+++ src/components/EventHeader.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import type { CollectionEntry } from 'astro:content';
+  import { format, formatDistance, fromUnixTime } from 'date-fns';
+
+  export let event: CollectionEntry<'events'>;
+  export let now: Date;
+  export let clickable: boolean = false;
+  export let relative: 'start' | 'end' | undefined = undefined;
+
+  const startDate = fromUnixTime(event.data.start);
+  const endDate = fromUnixTime(event.data.end);
+</script>
+
+{#if clickable}
+  <a href={`/events/${event.slug}`}>
+    <h2>{event.data.name}</h2>
+  </a>
+{:else}
+  <h2>{event.data.name}</h2>
+{/if}
+<h5>{event.data.description}</h5>
+
+{#if relative === 'start'}
+  <p>
+    {`Starts ${formatDistance(startDate, now, {
+      addSuffix: true
+    })} - Ends at ${format(endDate, 'P')}`}
+  </p>
+{:else if relative === 'end'}
+  <p>
+    {`Started at ${format(startDate, 'P')} - Ends ${formatDistance(endDate, now, {
+      addSuffix: true
+    })}`}
+  </p>
+{:else}
+  <p>{`Started ${format(startDate, 'P')} - Ended ${format(endDate, 'P')}`}</p>
+{/if}
