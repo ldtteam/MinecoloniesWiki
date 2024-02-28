3cd41f12e60fecf2d39520248d205fb6b97dcd04
Thom van den Akker <thodor20@gmail.com>
2024-02-28 22:13:39 +0100

    Removal of old stuff

--- pages/src/_plugins/utils/require.rb
+++
@@ -1,7 +0,0 @@
-require_relative "building_utils"
-require_relative "common_utils"
-require_relative "content_renderer"
-require_relative "number_utils"
-require_relative "research_lookup"
-require_relative "version_renderer"
-require_relative "worker_utils"
