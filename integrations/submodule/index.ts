df015d27e1353b8e0ff5763e5f4ae0315c99edf2
Thom van den Akker <thodor20@gmail.com>
2024-03-08 13:05:31 +0100

    Add submodule

--- integrations/submodule/index.ts
+++ integrations/submodule/index.ts
@@ -1,4 +1,5 @@
 import { AstroIntegration } from 'astro';
+import shelljs from 'shelljs';
 
 import { initSubmodule } from './update-minecolonies';
 
@@ -14,6 +15,12 @@ export function minecoloniesSubmodule(): AstroIntegration {
     hooks: {
       'astro:config:setup': async () => {
         const module = await initSubmodule(url, name);
+
+        // shelljs.cd(module.path());
+        // shelljs.exec('git sparse-checkout init --cone --sparse-index');
+        // for (const directory of directoriesToPull) {
+        //   shelljs.exec(`git sparse-checkout set '${directory}'`);
+        // }
 
         await module.update(1);
       }
--- integrations/submodule/update-minecolonies.ts
+++ integrations/submodule/update-minecolonies.ts
@@ -1,17 +1,11 @@
 import Nodegit from 'nodegit';
-import shelljs from 'shelljs';
-
-const directoriesToPull = ['src/datagen/generated/minecolonies/data/minecolonies/researches'];
 
 export async function initSubmodule(url: string, name: string): Promise<Nodegit.Submodule> {
   const repo = await Nodegit.Repository.open(process.cwd());
-  const module = await Nodegit.Submodule.lookup(repo, name);
 
-  shelljs.cd(module.path());
-  shelljs.exec('git sparse-checkout init --cone --sparse-index');
-  for (const directory of directoriesToPull) {
-    shelljs.exec(`git sparse-checkout set '${directory}'`);
-  }
-
-  return module;
+  return await Nodegit.Submodule.lookup(repo, name);
+  // await module.init(0);
+  // await module.addToIndex(1);
+  // await module.open().then((subRepo) => subRepo.fetch('origin'));
+  // return module;
 }
