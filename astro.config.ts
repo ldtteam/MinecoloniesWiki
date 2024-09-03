05e3cbe08bf510d2573c5f2327c00cd719ca68e8
Thom van den Akker <thodor20@gmail.com>
2024-09-03 10:03:59 +0200

    - Fix cheerio startup error - Update astro - Split release and build pipeline - Remove Squoosh in favor of Sharp image processor (should work with GH actions) - Fix table styling on tag tool page

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import markdoc from '@astrojs/markdoc';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import purgecss from 'astro-purgecss';
 
@@ -12,8 +12,7 @@ export default defineConfig({
   output: 'static',
   site: 'https://minecolonies.com',
   image: {
-    domains: ['minecraft.wiki'],
-    service: squooshImageService()
+    domains: ['minecraft.wiki']
   },
   integrations: [
     svelte(),
