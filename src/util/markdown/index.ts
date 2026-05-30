2a958b51f40c3e37af79b85f569654088f4396c1
Thom van den Akker <thodor20@gmail.com>
2026-05-30 19:53:58 +0200

    Fixes

--- src/util/markdown/index.ts
+++ src/util/markdown/index.ts
@@ -1,12 +1,21 @@
 import { createMarkdownProcessor } from '@astrojs/markdown-remark';
 import type { CollectionEntry } from 'astro:content';
+import type { TextDirective } from 'mdast-util-directive';
+import type { State } from 'mdast-util-to-hast';
 import remarkDirective from 'remark-directive';
 
 import { rehypeBuildingPlugin } from './rehype/buildings';
 
 const processor = createMarkdownProcessor({
   remarkPlugins: [remarkDirective],
-  rehypePlugins: [rehypeBuildingPlugin]
+  rehypePlugins: [rehypeBuildingPlugin],
+  remarkRehype: {
+    handlers: {
+      textDirective: (state: State, node: TextDirective) => {
+        return { type: 'element', tagName: node.name, properties: node.attributes ?? {}, children: state.all(node) };
+      }
+    }
+  }
 });
 
 export async function formatMarkdownText(text: string, frontmatter?: CollectionEntry<'wiki'>['data']) {
